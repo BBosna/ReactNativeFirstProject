a9cddd15edd8a21f4219a23a038d85056f0a4cb7
Bedirhan Bosna <bedirhanbosna@tuta.io>
2023-01-19 01:26:08 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Card from './components/Card';
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Card title="Bedirhan Bosna" text="because" />
+      <Card title="Bedirhan Bosna" text="because" />
+      <Card title="Bedirhan Bosna" text="because" />
+      <Card title="Bedirhan Bosna" text="because" />
+      <Card title="Bedirhan Bosna" text="because" />
+    </SafeAreaView>
+  );
+};
+
+export default App;
